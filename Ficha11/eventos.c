f8b5365068cb3ee82a17b08f7d45cddb1f9b3005
Francisco Araújo <quico.calado.araujo@gmail.com>
2020-06-24 19:45:06 +0100

    Finished ex2, kinda.

--- Ficha11/eventos.c
+++ Ficha11/eventos.c
@@ -24,27 +24,97 @@ int pop_number(char *str) {
     return ret;
 }
 
+int say_number(char *str) {
+    int i = strlen(str) - 1;
+    while(i >= 0 && isspace(str[i])) i--;
+
+    if(i < 0)
+        return -1;
+
+    while(i >= 0 && !isspace(str[i])) i--;
+    i++;
+
+    int ret = atoi(str + i);
+
+    return ret;
+}
+
 
 int le_eventos(FILE *f, heap *eventos_agendados) {
     
     /* alinea 2.1 */
+    if(f == NULL){
+        return 0;
+    }
 
-    return 0;
+    if(eventos_agendados == NULL){
+        return 0;
+    }
+    
+    char palavra[MAX_LINE];
+    int nEventos=0, prioridade;
+
+    while(fgets(palavra, MAX_LINE, f) != NULL)
+    {
+        pop_number(palavra);
+        palavra[strlen(palavra)-1] = '\0';
+        
+        prioridade = say_number(palavra);
+
+        heap_insere(eventos_agendados, palavra, prioridade);
+        nEventos++;
+    }
+
+    return nEventos;
 }
 
 int processa_tempo(int tempo, heap *eventos_agendados, heap *eventos_espera) {
 
     /* alinea 2.2 */
+    
+    int duracao, prioridade,nEventos=0;
+    char *palavra;
+    heap *aux = heap_nova(MAX_HEAP);
 
-    return 0;
+    while((palavra = heap_remove(eventos_agendados)) != NULL)
+    {
+        prioridade = say_number(palavra);
+
+        duracao=say_number(palavra);
+        
+        if(duracao <= tempo){
+            prioridade = prioridade*-1;
+            pop_number(palavra);
+            palavra[strlen(palavra)-1] = '\0';
+            heap_insere(eventos_espera, palavra, prioridade);
+        } else {
+            heap_insere(aux, palavra, prioridade);
+        }
+        nEventos++;
+    }
+
+    while((palavra = heap_remove(aux)) != NULL)
+    {
+        heap_insere(eventos_agendados, palavra, prioridade);
+    }
+    
+    heap_apaga(aux);
+
+    return nEventos;
 }
 
 char* proximo_evento(heap *eventos_espera, int *duracao) {
 
     /* alinea 2.3 */
+    char *palavra;
 
+    if((palavra = heap_remove(eventos_espera)) == NULL){
+        return NULL;
+    }
+    
+    *duracao = pop_number(palavra);
 
-    return NULL;
+    return palavra;
 }
 
 
@@ -67,6 +137,8 @@ void processa_eventos(heap *eventos_agendados){
         else
             time = eventos_agendados->elementos[1]->prioridade;
     }
+
+    
 
     heap_apaga(eventos_espera);
 }
