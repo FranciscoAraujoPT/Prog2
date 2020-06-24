94246d3730bd39608c6ea7b73024bc4b79cf29ab
Francisco Araújo <quico.calado.araujo@gmail.com>
2020-06-24 15:22:54 +0100

    Work in progress of Ficha 11

--- Ficha11/dict.c
+++ Ficha11/dict.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 
 #define MAX_WORD 256
@@ -23,21 +26,99 @@ void downcase (char * str)
 int le_dicionario(FILE * f, arvore_avl *dict) {
 
   /* alinea 1.1 */
-  return 0;
+  if(f == NULL){
+    return 0;
+  }
+
+  if(dict == NULL){
+    return 0;
+  }
+  
+  int nPalavras=0;
+  char palavra[MAX_WORD];
+
+  while(fgets(palavra, MAX_WORD, f) != NULL)
+  {
+    palavra[strlen(palavra)-1] = '\0';
+    downcase(palavra);
+    if(avl_insere(dict, palavra) == -1){
+      return 0;
+    }
+    nPalavras++;
+  }
+
+  return nPalavras;
 }
 
 char* isola_palavra(char *str, int *len) {
 
   /* alinea 1.2 */
-  return NULL;
+  if(str == NULL){
+    return NULL;
+  }
+
+  *len = 0;
+
+  char c;
+  char *palavra=NULL;
+
+  while((c=str[*len]) != '\0')
+  {
+    if(c == ' ' && (*len)>0){
+      break;
+    }
+
+    if(c != ' ' && palavra==NULL){
+      palavra=&str[*len];
+    }
+
+    (*len)++;
+  }
+
+  return palavra;
 }
 
 char* verifica_texto(arvore_avl * dict, char *str, int *len) {
 
   /* alinea 1.3 */
-  return NULL;
-}
+  if(dict == NULL){
+    return NULL;
+  }
 
+  if(str == NULL){
+    return NULL;
+  }
+
+  if(len == NULL){
+    return NULL;
+  }
+  
+  int i;
+  char *palavra=NULL;
+
+
+  while((palavra=isola_palavra(str, len)) != NULL)
+  {
+    char *aux2 = (char*) malloc(sizeof(char)*MAX_WORD);
+    
+
+    for(i=0;i<(*len);i++)
+    {
+      aux2[i]=palavra[i];
+    }
+    aux2[*len] = '\0';
+
+    downcase(aux2);
+
+    no_avl *aux = avl_pesquisa(dict, aux2);
+
+    if(aux == NULL){
+      return palavra;
+    }
+    str = &palavra[*len+1];
+  }
+  return palavra;
+}
 
 int main() {
     FILE * f = fopen("dict.txt", "r");
